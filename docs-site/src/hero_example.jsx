e2b323ce665924971a5cbd862ab11e2ce5e75067
Manfred Xu <manfredxu99@gmail.com>
2017-06-30 12:34:13 +0800

    Update hero_example.jsx

--- docs-site/src/hero_example.jsx
+++ docs-site/src/hero_example.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import DatePicker from '../../dist/react-datepicker.min.js'
+import DatePicker from 'react-datepicker'
 import moment from 'moment'
 
 export default class HeroExample extends React.Component {
